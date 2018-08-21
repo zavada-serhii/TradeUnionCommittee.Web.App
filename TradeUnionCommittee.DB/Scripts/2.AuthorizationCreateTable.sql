899513468e6e4848375b048c1eb3cfd02a3ddf8c
Sergey Zavada <zavadaserg@hotmail.com>
2018-08-21 23:27:45 +0300

    Added script for add default users in system

--- TradeUnionCommittee.DB/Scripts/2.AuthorizationCreateTable.sql
+++ TradeUnionCommittee.DB/Scripts/2.AuthorizationCreateTable.sql
@@ -14,3 +14,13 @@ CREATE TABLE "Users"(
 );
 ALTER TABLE "Users"
 OWNER TO AdminTradeUnionCommitteeEmployees;
+
+INSERT INTO "Roles" ("Name") VALUES
+('Admin'),
+('Accountant'),
+('Deputy');
+
+INSERT INTO "Users" ("IdRole","Email","Password") VALUES
+(1,'stewie.griffin@test.com','ACbdDpAP6Dwu1djzNCN2CpcP9U9Cctq3AalWMA6qipuF7Qf3SjNnXzeCZ1KJZj0aKQ=='),
+(2,'accountant@test.com','ACWKqqqrtM65KHIxy3LAVuq92NUgjnhfu7BBcn6sAVZhKagfU+5kzTq+c6CFA9M46w=='),
+(3,'deputy@test.com','AFwdvz+/flWC6PP+AjwcsZeH+Oos7iKras6k0Qq+48srGTI/z5oUHe5+k3SDFkfjHg==');
