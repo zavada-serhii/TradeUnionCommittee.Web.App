d1b6a559dcf58f1204c3a6a802eb4f93c07d82f6
DESKTOP-OLS7JFG\Home <zavadaserg@hotmail.com>
2018-09-15 23:29:54 +0300

    removed unused view model

--- TradeUnionCommittee.Web.GUI/Models/AccountViewModel.cs
+++ TradeUnionCommittee.Web.GUI/Models/AccountViewModel.cs
@@ -61,18 +61,6 @@ namespace TradeUnionCommittee.Web.GUI.Models
         public string ConfirmPassword { get; set; }
     }
 
-    public class UpdatePersonalDataAccountViewModel
-    {
-        public long? IdUser { get; set; }
-
-        [Required(ErrorMessage = "Email не може бути порожнім")]
-        [EmailAddress(ErrorMessage = "Некоректний Email")]
-        [Remote("CheckEmail", "Account", ErrorMessage = "Цей email вже використовується!")]
-        public string Email { get; set; }
-
-        public long IdRole { get; set; }
-    }
-
     public class UpdateRoleAccountViewModel
     {
         public string HashIdUser { get; set; }
