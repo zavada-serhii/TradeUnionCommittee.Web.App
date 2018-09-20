b3a0e9db3f30b51ce6c1c8e8c09c9e36ce0d289c
DESKTOP-OLS7JFG\Home <zavadaserg@hotmail.com>
2018-09-21 02:07:54 +0300

    Added attribute ConcurrencyCheck in entities Activities

--- TradeUnionCommittee.DAL/Entities/Activities.cs
+++ TradeUnionCommittee.DAL/Entities/Activities.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TradeUnionCommittee.DAL.Entities
 {
@@ -13,6 +14,7 @@ namespace TradeUnionCommittee.DAL.Entities
         }
 
         public long Id { get; set; }
+        [ConcurrencyCheck]
         public string Name { get; set; }
 
         public ICollection<ActivityChildrens> ActivityChildrens { get; set; }
