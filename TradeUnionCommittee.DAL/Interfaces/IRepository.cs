73eb01443c9287a893c1c58c725f4569920205fa
Sergey Zavada <zavadaserg@hotmail.com>
2018-11-25 23:41:03 +0200

    completed refatoring

--- TradeUnionCommittee.DAL/Interfaces/IRepository.cs
+++ TradeUnionCommittee.DAL/Interfaces/IRepository.cs
@@ -10,11 +10,11 @@ namespace TradeUnionCommittee.DAL.Interfaces
     {
         Task<ActualResult<IEnumerable<T>>> GetAll();
         Task<ActualResult<T>> Get(long id);
-        Task<ActualResult<IEnumerable<T>>> Find(Func<T, bool> predicate);
+        Task<ActualResult<IEnumerable<T>>> Find(Expression<Func<T, bool>> predicate);
         Task<ActualResult> Create(T item);
         Task<ActualResult> Update(T item);
         Task<ActualResult> Delete(long id);
         Task<ActualResult<IEnumerable<T>>> GetWithInclude(params Expression<Func<T, object>>[] includeProperties);
-        Task<ActualResult<IEnumerable<T>>> GetWithInclude(Func<T, bool> predicate, params Expression<Func<T, object>>[] includeProperties);
+        Task<ActualResult<IEnumerable<T>>> GetWithInclude(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
     }
 }
