48cf2b94ce7ec8746ceb1842b37263ee5b05d30f
Sergey Zavada <zavadaserg@hotmail.com>
2018-12-27 21:19:52 +0200

    cosmetic fix

--- TradeUnionCommittee.BLL/Interfaces/Lists/IPositionEmployeesService.cs
+++ TradeUnionCommittee.BLL/Interfaces/Lists/IPositionEmployeesService.cs
@@ -1,10 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using TradeUnionCommittee.BLL.DTO;
 using TradeUnionCommittee.Common.ActualResults;
 
 namespace TradeUnionCommittee.BLL.Interfaces.Lists
 {
-    public interface IPositionEmployeesService
+    public interface IPositionEmployeesService : IDisposable
     {
         Task<ActualResult<PositionEmployeesDTO>> GetAsync(string hashIdEmployee);
         Task<ActualResult> UpdateAsync(PositionEmployeesDTO dto);
