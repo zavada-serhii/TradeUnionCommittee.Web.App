19d816cb93c21a258ee9081baf3787f11ad0a47b
Sergey Zavada <zavadaserg@hotmail.com>
2019-02-14 18:57:14 +0200

    Fixed bug with divider path for target platform

--- TradeUnionCommittee.PDF.Service/BaseSettings.cs
+++ TradeUnionCommittee.PDF.Service/BaseSettings.cs
@@ -9,7 +9,7 @@ namespace TradeUnionCommittee.PDF.Service
     public class BaseSettings
     {
         private static readonly string BasePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        private readonly string _pathToContainer = $@"{BasePath}\PDF Container";
+        private readonly string _pathToContainer;
         protected readonly string PathToFile;
         protected readonly Font Font;
         protected readonly Font FontBold;
@@ -17,11 +17,13 @@ namespace TradeUnionCommittee.PDF.Service
 
         protected BaseSettings()
         {
-            var baseFont = BaseFont.CreateFont($@"{BasePath}\Fonts\TimesNewRoman.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+            var pathDivider = GetPathDividerForTargetPlatform();
+            _pathToContainer = $@"{BasePath}{pathDivider}PDF Container";
+            var baseFont = BaseFont.CreateFont($@"{BasePath}{pathDivider}Fonts{pathDivider}TimesNewRoman.ttf", BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
             Font = new Font(baseFont, 14, Font.NORMAL);
             FontBold = new Font(baseFont, 12, Font.BOLD);
             CheckDirectory();
-            PathToFile = $@"{_pathToContainer}\{Guid.NewGuid()}.pdf";
+            PathToFile = $@"{_pathToContainer}{pathDivider}{Guid.NewGuid()}.pdf";
         }
 
         protected void AddEmptyParagraph(IElementListener document, int count)
@@ -48,6 +50,23 @@ namespace TradeUnionCommittee.PDF.Service
             if (!dirInfo.Exists)
             {
                 dirInfo.Create();
+            }
+        }
+
+        private char GetPathDividerForTargetPlatform()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                case PlatformID.Win32NT:
+                case PlatformID.WinCE:
+                    return '\\';
+                case PlatformID.Unix:
+                case PlatformID.MacOSX:
+                    return '/';
+                default:
+                    throw new ArgumentOutOfRangeException();
             }
         }
     }
