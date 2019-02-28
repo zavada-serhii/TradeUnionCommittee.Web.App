c7391f8a71a2e087bc43772345439a27061cdf53
Sergey Zavada <zavadasergey@outlook.com>
2019-02-28 18:03:11 +0200

    Added Travel, Wellness, Tour, Activity and Cultural controllers and Views for Family, added View Models and configuration auto mapper for this controllers

---
+++ TradeUnionCommittee.ViewModels/ViewModels/ActivityFamilyViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TradeUnionCommittee.ViewModels.ViewModels
+{
+    public class CreateActivityFamilyViewModel
+    {
+        [Required]
+        public string HashIdFamily { get; set; }
+        [Required(ErrorMessage = "Назва заходу не може бути порожньою!")]
+        public string HashIdActivities { get; set; }
+        [Required(ErrorMessage = "Дата проведення не може бути порожньою!")]
+        public DateTime DateEvent { get; set; }
+    }
+
+    public class UpdateActivityFamilyViewModel : CreateActivityFamilyViewModel
+    {
+        [Required]
+        public string HashId { get; set; }
+        [Required]
+        public uint RowVersion { get; set; }
+    }
+}
