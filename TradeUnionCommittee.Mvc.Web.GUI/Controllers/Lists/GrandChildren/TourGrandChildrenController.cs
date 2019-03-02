6a9c27b944ae466b694b135dd4069f2a154327db
DESKTOP-OLS7JFG\Home <zavadaserg@hotmail.com>
2019-03-03 01:36:36 +0200

    Added empty controllers and configure routings for Grand Childrens

---
+++ TradeUnionCommittee.Mvc.Web.GUI/Controllers/Lists/GrandChildren/TourGrandChildrenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradeUnionCommittee.Mvc.Web.GUI.Controllers.Lists.GrandChildren
+{
+    public class TourGrandChildrenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TradeUnionCommittee.Mvc.Web.GUI/Controllers/Lists/GrandChildren/TravelGrandChildrenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TradeUnionCommittee.Mvc.Web.GUI.Controllers.Lists.GrandChildren
+{
+    public class TravelGrandChildrenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
