2dd9d5ad60e5df94bd53e8170c5003e0ecc16576
DESKTOP-OLS7JFG\Home <zavadaserg@hotmail.com>
2019-03-02 23:23:41 +0200

    Controller refactoring

---
+++ TradeUnionCommittee.Mvc.Web.GUI/Controllers/Lists/Employee/ApartmentAccountingEmployeesController.cs
@@ -0,0 +1,145 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TradeUnionCommittee.BLL.DTO.Employee;
+using TradeUnionCommittee.BLL.Enums;
+using TradeUnionCommittee.BLL.Interfaces.Lists;
+using TradeUnionCommittee.BLL.Interfaces.SystemAudit;
+using TradeUnionCommittee.Mvc.Web.GUI.Extensions;
+using TradeUnionCommittee.ViewModels.ViewModels.Employee;
+
+namespace TradeUnionCommittee.Mvc.Web.GUI.Controllers.Lists.Employee
+{
+    public class ApartmentAccountingEmployeesController : Controller
+    {
+        private readonly IApartmentAccountingEmployeesService _services;
+        private readonly IMapper _mapper;
+        private readonly ISystemAuditService _systemAuditService;
+        private readonly IHttpContextAccessor _accessor;
+
+        public ApartmentAccountingEmployeesController(IApartmentAccountingEmployeesService services, IMapper mapper, ISystemAuditService systemAuditService, IHttpContextAccessor accessor)
+        {
+            _services = services;
+            _mapper = mapper;
+            _systemAuditService = systemAuditService;
+            _accessor = accessor;
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+        [HttpGet]
+        [Authorize(Roles = "Admin,Accountant,Deputy")]
+        public async Task<IActionResult> Index([Required] string id)
+        {
+            var result = await _services.GetAllAsync(id);
+            if (result.IsValid)
+            {
+                ViewData["HashIdEmployee"] = id;
+                return View(result.Result);
+            }
+            TempData["ErrorsList"] = result.ErrorsList;
+            return View();
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+        [HttpGet]
+        [Authorize(Roles = "Admin,Accountant,Deputy")]
+        public IActionResult Create([Required] string id)
+        {
+            return View(new CreateApartmentAccountingEmployeesViewModel { HashIdEmployee = id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin,Accountant,Deputy")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateApartmentAccountingEmployeesViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _services.CreateAsync(_mapper.Map<ApartmentAccountingEmployeesDTO>(vm));
+                if (result.IsValid)
+                {
+                    await _systemAuditService.AuditAsync(User.GetEmail(), _accessor.GetIp(), Operations.Insert, Tables.ApartmentAccountingEmployees);
+                    return RedirectToAction("Index", new { id = vm.HashIdEmployee });
+                }
+                TempData["ErrorsList"] = result.ErrorsList;
+            }
+            return View(vm);
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+        [HttpGet]
+        [Authorize(Roles = "Admin,Accountant,Deputy")]
+        public async Task<IActionResult> Update([Required] string id)
+        {
+            var result = await _services.GetAsync(id);
+            if (result.IsValid)
+            {
+                return View(_mapper.Map<UpdateApartmentAccountingEmployeesViewModel>(result.Result));
+            }
+            TempData["ErrorsList"] = result.ErrorsList;
+            return View();
+        }
+
+        [HttpPost, ActionName("Update")]
+        [Authorize(Roles = "Admin,Accountant,Deputy")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(UpdateApartmentAccountingEmployeesViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _services.UpdateAsync(_mapper.Map<ApartmentAccountingEmployeesDTO>(vm));
+                if (result.IsValid)
+                {
+                    await _systemAuditService.AuditAsync(User.GetEmail(), _accessor.GetIp(), Operations.Update, Tables.ApartmentAccountingEmployees);
+                    return RedirectToAction("Index", new { id = vm.HashIdEmployee });
+                }
+                TempData["ErrorsListConfirmed"] = result.ErrorsList;
+            }
+            return View(vm);
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete([Required] string id)
+        {
+            var result = await _services.GetAsync(id);
+            if (result.IsValid)
+            {
+                return View(result.Result);
+            }
+            TempData["ErrorsList"] = result.ErrorsList;
+            return View();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed([Required] string hashId, [Required] string hashIdEmployee)
+        {
+            var result = await _services.DeleteAsync(hashId);
+            if (result.IsValid)
+            {
+                await _systemAuditService.AuditAsync(User.GetEmail(), _accessor.GetIp(), Operations.Delete, Tables.ApartmentAccountingEmployees);
+                return RedirectToAction("Index", new { id = hashIdEmployee });
+            }
+            TempData["ErrorsList"] = result.ErrorsList;
+            return View();
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+        protected override void Dispose(bool disposing)
+        {
+            _services.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TradeUnionCommittee.Mvc.Web.GUI/Controllers/Lists/Family/ActivityFamilyController.cs
@@ -0,0 +1,156 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TradeUnionCommittee.BLL.DTO.Family;
+using TradeUnionCommittee.BLL.Enums;
+using TradeUnionCommittee.BLL.Interfaces.Helpers;
+using TradeUnionCommittee.BLL.Interfaces.Lists.Family;
+using TradeUnionCommittee.BLL.Interfaces.SystemAudit;
+using TradeUnionCommittee.Mvc.Web.GUI.Controllers.Directory;
+using TradeUnionCommittee.Mvc.Web.GUI.Extensions;
+using TradeUnionCommittee.ViewModels.ViewModels.Family;
+
+namespace TradeUnionCommittee.Mvc.Web.GUI.Controllers.Lists.Family
+{
+    public class ActivityFamilyController : Controller
+    {
+        private readonly IActivityFamilyService _services;
+        private readonly IDirectories _directories;
+        private readonly IMapper _mapper;
+        private readonly ISystemAuditService _systemAuditService;
+        private readonly IHttpContextAccessor _accessor;
+        private readonly IReferenceParent _referenceParent;
+
+        public ActivityFamilyController(IActivityFamilyService services, IDirectories directories, IMapper mapper, ISystemAuditService systemAuditService, IHttpContextAccessor accessor, IReferenceParent referenceParent)
+        {
+            _services = services;
+            _mapper = mapper;
+            _systemAuditService = systemAuditService;
+            _accessor = accessor;
+            _referenceParent = referenceParent;
+            _directories = directories;
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+        [HttpGet]
+        [Authorize(Roles = "Admin,Accountant,Deputy")]
+        public async Task<IActionResult> Index([Required] string id)
+        {
+            var result = await _services.GetAllAsync(id);
+            if (result.IsValid)
+            {
+                ViewData["HashIdFamily"] = id;
+                ViewData["HashIdEmployee"] = await _referenceParent.GetHashIdEmployeeByFamily(id);
+                return View(result.Result);
+            }
+            TempData["ErrorsList"] = result.ErrorsList;
+            return View();
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+        [HttpGet]
+        [Authorize(Roles = "Admin,Accountant,Deputy")]
+        public async Task<IActionResult> Create([Required] string id)
+        {
+            ViewBag.Activities = await _directories.GetActivities();
+            return View(new CreateActivityFamilyViewModel { HashIdFamily = id });
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Admin,Accountant,Deputy")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateActivityFamilyViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _services.CreateAsync(_mapper.Map<ActivityFamilyDTO>(vm));
+                if (result.IsValid)
+                {
+                    await _systemAuditService.AuditAsync(User.GetEmail(), _accessor.GetIp(), Operations.Insert, Tables.ActivityFamily);
+                    return RedirectToAction("Index", new { id = vm.HashIdFamily });
+                }
+                TempData["ErrorsList"] = result.ErrorsList;
+            }
+            ViewBag.Activities = await _directories.GetActivities();
+            return View(vm);
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+        [HttpGet]
+        [Authorize(Roles = "Admin,Accountant,Deputy")]
+        public async Task<IActionResult> Update([Required] string id)
+        {
+            var result = await _services.GetAsync(id);
+            if (result.IsValid)
+            {
+                ViewBag.Activities = await _directories.GetActivities(result.Result.HashIdActivities);
+                return View(_mapper.Map<UpdateActivityFamilyViewModel>(result.Result));
+            }
+            TempData["ErrorsList"] = result.ErrorsList;
+            return View();
+        }
+
+        [HttpPost, ActionName("Update")]
+        [Authorize(Roles = "Admin,Accountant,Deputy")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(UpdateActivityFamilyViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _services.UpdateAsync(_mapper.Map<ActivityFamilyDTO>(vm));
+                if (result.IsValid)
+                {
+                    await _systemAuditService.AuditAsync(User.GetEmail(), _accessor.GetIp(), Operations.Update, Tables.ActivityFamily);
+                    return RedirectToAction("Index", new { id = vm.HashIdFamily });
+                }
+                TempData["ErrorsListConfirmed"] = result.ErrorsList;
+            }
+            ViewBag.Activities = await _directories.GetActivities(vm.HashIdActivities);
+            return View(vm);
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete([Required] string id)
+        {
+            var result = await _services.GetAsync(id);
+            if (result.IsValid)
+            {
+                return View(result.Result);
+            }
+            TempData["ErrorsList"] = result.ErrorsList;
+            return View();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [Authorize(Roles = "Admin")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed([Required] string hashId, [Required] string hashIdFamily)
+        {
+            var result = await _services.DeleteAsync(hashId);
+            if (result.IsValid)
+            {
+                await _systemAuditService.AuditAsync(User.GetEmail(), _accessor.GetIp(), Operations.Delete, Tables.ActivityFamily);
+                return RedirectToAction("Index", new { id = hashIdFamily });
+            }
+            TempData["ErrorsList"] = result.ErrorsList;
+            return View();
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------------------
+
+        protected override void Dispose(bool disposing)
+        {
+            _services.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
