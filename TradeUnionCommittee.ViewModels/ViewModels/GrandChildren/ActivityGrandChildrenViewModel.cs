15cdf3f651eb27682e72e472162a13c15a791a33
DESKTOP-OLS7JFG\Home <zavadaserg@hotmail.com>
2019-03-03 01:30:24 +0200

    Added view models, configure auto mapper and added empty view for Grand Children

---
+++ TradeUnionCommittee.ViewModels/ViewModels/GrandChildren/ActivityGrandChildrenViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TradeUnionCommittee.ViewModels.ViewModels.GrandChildren
+{
+    public class CreateActivityGrandChildrenViewModel
+    {
+        [Required]
+        public string HashIdGrandChildren { get; set; }
+        [Required(ErrorMessage = "Назва заходу не може бути порожньою!")]
+        public string HashIdActivities { get; set; }
+        [Required(ErrorMessage = "Дата проведення не може бути порожньою!")]
+        public DateTime DateEvent { get; set; }
+    }
+
+    public class UpdateActivityGrandChildrenViewModel : CreateActivityGrandChildrenViewModel
+    {
+        [Required]
+        public string HashId { get; set; }
+        [Required]
+        public uint RowVersion { get; set; }
+    }
+}
