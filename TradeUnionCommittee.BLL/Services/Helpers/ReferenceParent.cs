d0c5425e2811923e1d6157b183141baeb8e738a4
Sergey Zavada <zavadasergey@outlook.com>
2019-03-27 11:48:19 +0200

    Refactoring ReferenceParent and PdfService

--- TradeUnionCommittee.BLL/Services/Helpers/ReferenceParent.cs
+++ TradeUnionCommittee.BLL/Services/Helpers/ReferenceParent.cs
@@ -1,57 +1,45 @@
 ﻿using System.Threading.Tasks;
 using TradeUnionCommittee.BLL.Configurations;
 using TradeUnionCommittee.BLL.Interfaces.Helpers;
-using TradeUnionCommittee.DAL.Interfaces;
+using TradeUnionCommittee.DAL.EF;
 
 namespace TradeUnionCommittee.BLL.Services.Helpers
 {
     public class ReferenceParent : IReferenceParent
     {
-        private readonly IUnitOfWork _database;
+        private readonly TradeUnionCommitteeContext _context;
         private readonly IHashIdConfiguration _hashIdUtilities;
 
-        public ReferenceParent(IUnitOfWork database, IHashIdConfiguration hashIdUtilities)
+        public ReferenceParent(TradeUnionCommitteeContext context, IHashIdConfiguration hashIdUtilities)
         {
-            _database = database;
+            _context = context;
             _hashIdUtilities = hashIdUtilities;
         }
 
         public async Task<string> GetHashIdEmployeeByFamily(string hashIdFamily)
         {
             var id = _hashIdUtilities.DecryptLong(hashIdFamily, Enums.Services.Family);
-            var result = await _database.FamilyRepository.GetById(id);
-            if (result.IsValid && result.Result != null)
-            {
-                return _hashIdUtilities.EncryptLong(result.Result.IdEmployee, Enums.Services.Employee);
-            }
-            return null;
+            var result = await _context.Family.FindAsync(id);
+            return result != null ? _hashIdUtilities.EncryptLong(result.IdEmployee, Enums.Services.Employee) : null;
         }
 
         public async Task<string> GetHashIdEmployeeByChildren(string hashIdChildren)
         {
             var id = _hashIdUtilities.DecryptLong(hashIdChildren, Enums.Services.Children);
-            var result = await _database.ChildrenRepository.GetById(id);
-            if (result.IsValid && result.Result != null)
-            {
-                return _hashIdUtilities.EncryptLong(result.Result.IdEmployee, Enums.Services.Employee);
-            }
-            return null;
+            var result = await _context.Children.FindAsync(id);
+            return result != null ? _hashIdUtilities.EncryptLong(result.IdEmployee, Enums.Services.Employee) : null;
         }
 
         public async Task<string> GetHashIdEmployeeByGrandChildren(string hashIdGrandChildren)
         {
             var id = _hashIdUtilities.DecryptLong(hashIdGrandChildren, Enums.Services.GrandChildren);
-            var result = await _database.GrandChildrenRepository.GetById(id);
-            if (result.IsValid && result.Result != null)
-            {
-                return _hashIdUtilities.EncryptLong(result.Result.IdEmployee, Enums.Services.Employee);
-            }
-            return null;
+            var result = await _context.GrandChildren.FindAsync(id);
+            return result != null ? _hashIdUtilities.EncryptLong(result.IdEmployee, Enums.Services.Employee) : null;
         }
 
         public void Dispose()
         {
-            _database.Dispose();
+            _context.Dispose();
         }
     }
 }
