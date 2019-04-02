6967f90dccb9a545772ff545fe14555babb2f0ce
Sergey Zavada <zavadaserg@hotmail.com>
2019-04-03 01:03:31 +0300

    Updated indentity migration

--- src/TradeUnionCommittee.DAL/Entities/User.cs
+++ src/TradeUnionCommittee.DAL/Entities/User.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TradeUnionCommittee.DAL.Entities
 {
     public class User : IdentityUser
     {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
         [NotMapped]
         public string Password { get; set; }
         [NotMapped]
