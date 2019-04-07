1d83a67e94249d08a1ef8d5cf50f2bb24015f1b1
DESKTOP-OLS7JFG\Home <zavadaserg@hotmail.com>
2019-04-07 23:34:48 +0300

    Made frame for error handling

---
+++ src/TradeUnionCommittee.BLL/Helpers/DescriptionExceptionHelper.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System;
+using System.Net.Sockets;
+using TradeUnionCommittee.Common.Enums;
+
+namespace TradeUnionCommittee.BLL.Helpers
+{
+    internal class DescriptionExceptionHelper
+    {
+        public static Errors GetDescriptionError(Exception exception)
+        {
+            if (exception.InnerException != null)
+            {
+                if (exception.InnerException is NpgsqlException npgsqlException && npgsqlException.InnerException is SocketException socketException)
+                {
+                    if (socketException.SocketErrorCode == SocketError.ConnectionReset)
+                    {
+                        return Errors.ConnectionLost;
+                    }
+                }
+
+                if (exception.InnerException is PostgresException postgresException)
+                {
+                    switch (postgresException.SqlState)
+                    {
+                        case "23503":
+                            return Errors.DataUsed;
+                        case "23505":
+                            return Errors.DuplicateData;
+                        default:
+                            return Errors.DataBaseError;
+                    }
+                }
+            }
+
+            if (exception is DbUpdateConcurrencyException)
+            {
+                return Errors.TupleDeletedOrUpdated;
+            }
+
+            return Errors.DataBaseError;
+        }
+    }
+}
--- src/TradeUnionCommittee.Common/Enums/Errors.cs
+++ src/TradeUnionCommittee.Common/Enums/Errors.cs
@@ -2,15 +2,15 @@
 {
     public enum Errors
     {
-        TupleDeleted = 1,
-        TupleDeletedOrUpdated = 3,
-        DuplicateData = 4,
-        InvalidLoginOrPassword = 5,
-        DataBaseError = 6,
-        NotFound = 7,
-        FileNotFound = 8,
-
-        UserNotFound = 9,
-        DuplicateEmail = 10
+        ConnectionLost,
+        DataBaseError,
+        UserNotFound,
+        DuplicateEmail,
+        DuplicateData,
+        TupleDeletedOrUpdated,
+        TupleDeleted,
+        DataUsed,
+        NotFound,
+        FileNotFound
     }
 }
