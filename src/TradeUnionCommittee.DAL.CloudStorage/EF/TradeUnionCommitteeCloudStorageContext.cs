75c121c8e88ac29fd1a63ddeaf70ba3a9617a092
Sergey Zavada <zavadaserg@hotmail.com>
2019-04-18 22:12:03 +0300

    Completed configure DAL for cloud storage

---
+++ src/TradeUnionCommittee.DAL.CloudStorage/EF/TradeUnionCommitteeCloudStorageContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TradeUnionCommittee.DAL.CloudStorage.Entities;
+
+namespace TradeUnionCommittee.DAL.CloudStorage.EF
+{
+    public sealed class TradeUnionCommitteeCloudStorageContext : DbContext
+    {
+        public TradeUnionCommitteeCloudStorageContext(DbContextOptions<TradeUnionCommitteeCloudStorageContext> options): base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<PdfBucket> PdfBucket { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.0-rtm-35687");
+
+            modelBuilder.Entity<PdfBucket>(entity =>
+            {
+                entity.Property(e => e.DateFrom).HasColumnType("date");
+
+                entity.Property(e => e.DateTo).HasColumnType("date");
+
+                entity.Property(e => e.EmailUser)
+                    .IsRequired()
+                    .HasColumnType("character varying");
+
+                entity.Property(e => e.FileName)
+                    .IsRequired()
+                    .HasColumnType("character varying");
+            });
+        }
+    }
+}
---
+++ src/TradeUnionCommittee.DAL.CloudStorage/Entities/PdfBucket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace TradeUnionCommittee.DAL.CloudStorage.Entities
+{
+    public class PdfBucket
+    {
+        public long Id { get; set; }
+        public long IdEmployee { get; set; }
+        public string FileName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string EmailUser { get; set; }
+        public ValueTuple<IPAddress, int> IpUser { get; set; }
+        public int TypeReport { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
