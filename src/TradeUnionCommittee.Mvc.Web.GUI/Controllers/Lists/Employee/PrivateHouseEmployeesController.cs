7b8ed953f58699ba35906a19cd673e79195a6020
Sergey Zavada <zavadaserg@hotmail.com>
2019-07-11 09:30:19 +0300

    Replaced the delete pages in private and public house with a modal dialog

--- src/TradeUnionCommittee.Mvc.Web.GUI/Controllers/Lists/Employee/PrivateHouseEmployeesController.cs
+++ src/TradeUnionCommittee.Mvc.Web.GUI/Controllers/Lists/Employee/PrivateHouseEmployeesController.cs
@@ -105,33 +105,17 @@ namespace TradeUnionCommittee.Mvc.Web.GUI.Controllers.Lists.Employee
         }
 
         //------------------------------------------------------------------------------------------------------------------------------------------
-
-        [HttpGet]
+        
+        [HttpDelete]
         [Authorize(Roles = "Admin")]
         public async Task<IActionResult> Delete([Required] string id)
         {
-            var result = await _services.GetAsync(id);
-            if (result.IsValid)
-            {
-                return View(result.Result);
-            }
-            TempData["ErrorsList"] = result.ErrorsList;
-            return View();
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [Authorize(Roles = "Admin")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed([Required] string hashId, [Required] string hashIdEmployee)
-        {
-            var result = await _services.DeleteAsync(hashId);
+            var result = await _services.DeleteAsync(id);
             if (result.IsValid)
             {
                 await _systemAuditService.AuditAsync(User.GetEmail(), _accessor.GetIp(), Operations.Delete, Tables.PrivateHouseEmployees);
-                return RedirectToAction("Index", new { id = hashIdEmployee });
             }
-            TempData["ErrorsList"] = result.ErrorsList;
-            return View();
+            return Ok(result);
         }
 
         //------------------------------------------------------------------------------------------------------------------------------------------
