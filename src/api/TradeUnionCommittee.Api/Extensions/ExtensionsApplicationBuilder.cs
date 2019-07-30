69e309a2d5f431a3c83f4e76f8a2c4a81edef1c3
Sergey Zavada <zavadasergey@outlook.com>
2019-07-30 11:53:53 +0300

    small renamed

---
+++ src/api/TradeUnionCommittee.Api/Extensions/ExtensionsApplicationBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using TradeUnionCommittee.Api.Middleware;
+
+namespace TradeUnionCommittee.Api.Extensions
+{
+    public static class ExtensionsApplicationBuilder
+    {
+        public static IApplicationBuilder UseCustomMiddlewares(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ExceptionMiddleware>();
+            return app;
+        }
+    }
+}
---
+++ src/api/TradeUnionCommittee.Api/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Serilog;
+
+namespace TradeUnionCommittee.Api
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await CreateWebHostBuilder(args).Build().RunAsync();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseSerilog();
+    }
+}
