c4200d1430c500cb948e54688c8380e3b4c4b636
Sergey Zavada <zavadaserg@hotmail.com>
2019-09-02 21:53:32 +0300

    Small refactoring ExtensionsString

--- src/TradeUnionCommittee.Core/src/TradeUnionCommittee.BLL/Extensions/ExtensionsString.cs
+++ src/TradeUnionCommittee.Core/src/TradeUnionCommittee.BLL/Extensions/ExtensionsString.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using System.Linq;
 
 namespace TradeUnionCommittee.BLL.Extensions
 {
     public static class ExtensionsString
     {
-        public static string AddMaskForCityPhone(this string phone)
-        {
-            return phone.Insert(3, "-").Insert(6, "-");
-        }
+        public static string AddMaskForCityPhone(this string phone) => phone.Insert(3, "-").Insert(6, "-");
 
-        public static bool IsEqual(this string input, string value)
-        {
-           return input.Equals(value, StringComparison.InvariantCultureIgnoreCase);
-        }
+        /// <summary>
+        /// Returns equality ignore culture and case
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="array"></param>
+        /// <returns>bool</returns>
+        public static bool IsEqual(this string input, params string[] array) => array.Contains(input, StringComparer.InvariantCultureIgnoreCase);
     }
 }
