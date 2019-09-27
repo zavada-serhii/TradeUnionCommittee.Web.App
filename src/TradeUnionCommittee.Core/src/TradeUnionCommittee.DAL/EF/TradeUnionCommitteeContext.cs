2b776b5530da24c3db7b9b83adbd8f2a5fcf8a4b
Sergey Zavada <zavadasergey@outlook.com>
2019-09-27 15:57:57 +0300

    Updated ExtensionsDatabaseFacade

--- src/TradeUnionCommittee.Core/src/TradeUnionCommittee.DAL/EF/TradeUnionCommitteeContext.cs
+++ src/TradeUnionCommittee.Core/src/TradeUnionCommittee.DAL/EF/TradeUnionCommitteeContext.cs
@@ -944,7 +944,7 @@ namespace TradeUnionCommittee.DAL.EF
             const string sql = "CREATE EXTENSION \"pg_trgm\" SCHEMA public VERSION \"1.3\"; CREATE FUNCTION public.\"TrigramFullName\"(p public.\"Employee\") RETURNS TEXT LANGUAGE plpgsql IMMUTABLE AS $$ BEGIN RETURN lower(trim(coalesce(p.\"FirstName\",\'\') || \' \' ||coalesce(p.\"SecondName\",\'\') || \' \' ||coalesce(p.\"Patronymic\",\'\'))); EXCEPTION WHEN others THEN RAISE EXCEPTION \'%\', sqlerrm; END; $$; ALTER FUNCTION public.\"TrigramFullName\"(p public.\"Employee\") OWNER TO postgres; CREATE INDEX info_gist_idx ON public.\"Employee\" USING gist(public.\"TrigramFullName\"(\"Employee\") gist_trgm_ops); CREATE INDEX info_trgm_idx ON public.\"Employee\" USING gin(public.\"TrigramFullName\"(\"Employee\") gin_trgm_ops); ";
             using (var dr = Database.ExecuteSqlQuery(sql))
             {
-                dr.DbDataReader.Close();
+                dr.Close();
             }
         }
     }
