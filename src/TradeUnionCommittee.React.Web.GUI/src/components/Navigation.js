2386190b3758227a73ef40eb18784bf5179bae7d
Serhii Zavada <zavada.serhii@outlook.com>
2019-12-31 00:18:50 +0200

    Small refactoring

--- src/TradeUnionCommittee.React.Web.GUI/src/components/Navigation.js
+++ src/TradeUnionCommittee.React.Web.GUI/src/components/Navigation.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
+
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
@@ -13,9 +14,6 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-import { Link } from "react-router-dom";
-import { Route, Switch } from 'react-router-dom'
-import Container from '@material-ui/core/Container';
 
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
@@ -43,14 +41,9 @@ import StorageIcon from '@material-ui/icons/Storage';
 import SearchIcon from '@material-ui/icons/Search';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
-import CounterContainer from '../containers/CounterContainer'
-
 const drawerWidth = 280;
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-  },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
     transition: theme.transitions.create(['width', 'margin'], {
@@ -102,22 +95,12 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(0, 1),
     ...theme.mixins.toolbar,
   },
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
   nested: {
     paddingLeft: theme.spacing(4),
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  container: {
-    paddingTop: theme.spacing(7),
-    paddingBottom: theme.spacing(7)
   }
 }));
 
-export default function MiniDrawer() {
+export default function Navigation() {
   const classes = useStyles();
   const [openPanel, setOpenPanel] = React.useState(false);
   const [openDirectories, setOpenDirectories] = React.useState(false);
@@ -138,9 +121,7 @@ export default function MiniDrawer() {
   };
 
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-
+    <div>
       {/* --- Header start --- */}
       <AppBar position="fixed" className={clsx(classes.appBar, { [classes.appBarShift]: openPanel })}>
         <Toolbar>
@@ -332,39 +313,6 @@ export default function MiniDrawer() {
 
       </Drawer>
       {/* --- Menu end --- */}
-
-
-      {/* --- Content start --- */}
-      <main className={classes.content}>
-        <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
-          <Switch>
-            <Route path="/create-employee" component={CounterContainer} />
-            <Route path="/position" render={() => <div>Here will be page for Position</div>} />
-            <Route path="/social-position" render={() => <div>Here will be page for social-position</div>} />
-            <Route path="/privileges" render={() => <div>Here will be page for privileges</div>} />
-            <Route path="/award" render={() => <div>Here will be page for award</div>} />
-            <Route path="/material-aid" render={() => <div>Here will be page for material-aid</div>} />
-            <Route path="/hobby" render={() => <div>Here will be page for hobby</div>} />
-            <Route path="/travel" render={() => <div>Here will be page for travel</div>} />
-            <Route path="/wellness" render={() => <div>Here will be page for wellness</div>} />
-            <Route path="/tour" render={() => <div>Here will be page for tour</div>} />
-            <Route path="/activities" render={() => <div>Here will be page for activities</div>} />
-            <Route path="/cultural-activities" render={() => <div>Here will be page for cultural-activities</div>} />
-            <Route path="/subdivisions" render={() => <div>Here will be page for subdivisions</div>} />
-            <Route path="/departmental-housing" render={() => <div>Here will be page for departmental-housing</div>} />
-            <Route path="/dormitory" render={() => <div>Here will be page for dormitory</div>} />
-            <Route path="/users" render={() => <div>Here will be page for users</div>} />
-            <Route path="/dashboard" render={() => <div>Here will be page for dashboard</div>} />
-            <Route path="/action-log" render={() => <div>Here will be page for action-log</div>} />
-            <Route path="/search" render={() => <div>Here will be page for search</div>} />
-            <Route path="/logout" render={() => <div>Here will be page for logout</div>} />
-          </Switch>
-        </Container>
-      </main>
-      {/* --- Content end --- */}
-
-
     </div>
   );
 }
--- src/TradeUnionCommittee.React.Web.GUI/src/routes/index.js
+++ src/TradeUnionCommittee.React.Web.GUI/src/routes/index.js
@@ -1,20 +1,78 @@
 import React from 'react'
-import CounterContainer from '../containers/CounterContainer'
-import Navigation from '../components/Navigation'
+import { Route, Switch } from 'react-router-dom';
+import { Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import styled from '@emotion/styled'
-import { Router } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Navigation from '../components/Navigation'
+import CounterContainer from '../containers/CounterContainer'
 
 const Container = styled.div`
   text-align: center;
 `
 export const history = createBrowserHistory()
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  appBarSpacer: theme.mixins.toolbar,
+  container: {
+    paddingTop: theme.spacing(7),
+    paddingBottom: theme.spacing(7),
+    margin: theme.spacing(5)
+  }
+}));
+
 function Routes() {
+
+  const classes = useStyles();
+
   return (
     <Router history={history}>
       <Container>
-        <Navigation />
+        <div className={classes.root}>
+          <CssBaseline />
+          <Navigation />
+
+          {/* --- Content start --- */}
+          <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            <Container maxWidth="lg" className={classes.container}>
+              <Switch>
+                <Route path="/create-employee" component={CounterContainer} />
+                <Route path="/position" render={() => <div>Here will be page for Position</div>} />
+                <Route path="/social-position" render={() => <div>Here will be page for social-position</div>} />
+                <Route path="/privileges" render={() => <div>Here will be page for privileges</div>} />
+                <Route path="/award" render={() => <div>Here will be page for award</div>} />
+                <Route path="/material-aid" render={() => <div>Here will be page for material-aid</div>} />
+                <Route path="/hobby" render={() => <div>Here will be page for hobby</div>} />
+                <Route path="/travel" render={() => <div>Here will be page for travel</div>} />
+                <Route path="/wellness" render={() => <div>Here will be page for wellness</div>} />
+                <Route path="/tour" render={() => <div>Here will be page for tour</div>} />
+                <Route path="/activities" render={() => <div>Here will be page for activities</div>} />
+                <Route path="/cultural-activities" render={() => <div>Here will be page for cultural-activities</div>} />
+                <Route path="/subdivisions" render={() => <div>Here will be page for subdivisions</div>} />
+                <Route path="/departmental-housing" render={() => <div>Here will be page for departmental-housing</div>} />
+                <Route path="/dormitory" render={() => <div>Here will be page for dormitory</div>} />
+                <Route path="/users" render={() => <div>Here will be page for users</div>} />
+                <Route path="/dashboard" render={() => <div>Here will be page for dashboard</div>} />
+                <Route path="/action-log" render={() => <div>Here will be page for action-log</div>} />
+                <Route path="/search" render={() => <div>Here will be page for search</div>} />
+                <Route path="/logout" render={() => <div>Here will be page for logout</div>} />
+              </Switch>
+            </Container>
+          </main>
+          {/* --- Content end --- */}
+          
+        </div>
       </Container>
     </Router>
   )
