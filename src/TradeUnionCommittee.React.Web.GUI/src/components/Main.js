0e5b1c7fd32cf219c4c9bc34cd2309c9b7edcb63
Serhii Zavada <zavada.serhii@outlook.com>
2020-01-06 20:18:41 +0200

    Small refactor Main.js

--- src/TradeUnionCommittee.React.Web.GUI/src/components/Main.js
+++ src/TradeUnionCommittee.React.Web.GUI/src/components/Main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Router } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import Navigation from '../components/Navigation'
@@ -8,10 +9,21 @@ import Content from '../components/Content'
 
 export const history = createBrowserHistory()
 
+const useStyles = makeStyles(theme => ({
+    markup: {
+        display: 'flex', 
+        backgroundColor: '#ffffff' , 
+        backgroundImage: 'linear-gradient(315deg, #ffffff 0%, #d7e1ec 74%)'
+    }
+}));
+
 function Main() {
+
+    const classes = useStyles();
+
     return (
         <Router history={history}>
-            <div style={{ display: 'flex', backgroundColor: '#ffffff' , backgroundImage: 'linear-gradient(315deg, #ffffff 0%, #d7e1ec 74%)'}}>
+            <div className={classes.markup}>
                 <CssBaseline />
                 <Navigation />
                 <Content />
