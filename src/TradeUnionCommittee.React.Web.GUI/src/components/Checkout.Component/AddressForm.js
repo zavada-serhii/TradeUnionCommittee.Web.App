84a98b16519ec59a466f6ea6f9ae9da491d6d1db
Serhii Zavada <zavada.serhii@outlook.com>
2020-01-13 21:07:49 +0200

    Change stepper type

--- src/TradeUnionCommittee.React.Web.GUI/src/components/Checkout.Component/AddressForm.js
+++ src/TradeUnionCommittee.React.Web.GUI/src/components/Checkout.Component/AddressForm.js
@@ -62,7 +62,7 @@ export default function AddressForm() {
           />
         </Grid>
         <Grid item xs={12} sm={6}>
-          <TextField id="state" name="state" label="State/Province/Region" fullWidth />
+          <TextField id="state" name="state" label="State / Province / Region" fullWidth />
         </Grid>
         <Grid item xs={12} sm={6}>
           <TextField
--- src/TradeUnionCommittee.React.Web.GUI/src/components/Checkout.Component/Checkout.js
+++ src/TradeUnionCommittee.React.Web.GUI/src/components/Checkout.Component/Checkout.js
@@ -3,27 +3,35 @@ import { makeStyles } from '@material-ui/core/styles';
 import Stepper from '@material-ui/core/Stepper';
 import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
+import StepContent from '@material-ui/core/StepContent';
 import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+
 import AddressForm from './AddressForm';
 import PaymentForm from './PaymentForm';
 import Review from './Review';
 
 const useStyles = makeStyles(theme => ({
-  stepper: {
-    padding: theme.spacing(3, 0, 5),
-  },
-  buttons: {
-    display: 'flex',
-    justifyContent: 'flex-end',
+  root: {
+    width: '100%',
+    textAlign: 'left',
   },
   button: {
-    marginTop: theme.spacing(3),
-    marginLeft: theme.spacing(1),
+    marginTop: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  actionsContainer: {
+    marginBottom: theme.spacing(2),
+  },
+  resetContainer: {
+    padding: theme.spacing(3),
   },
 }));
 
-const steps = ['Shipping address', 'Payment details', 'Review your order'];
+function getSteps() {
+  return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
+}
 
 function getStepContent(step) {
   switch (step) {
@@ -38,62 +46,62 @@ function getStepContent(step) {
   }
 }
 
-export default function Checkout() {
+export default function VerticalLinearStepper() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
 
   const handleNext = () => {
-    setActiveStep(activeStep + 1);
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
   };
 
   const handleBack = () => {
-    setActiveStep(activeStep - 1);
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
   };
 
   return (
-    <div>
-      <Typography component="h1" variant="h4" align="center">
-        Checkout
-          </Typography>
-      <Stepper activeStep={activeStep} className={classes.stepper}>
-        {steps.map(label => (
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep} orientation="vertical">
+        {steps.map((label, index) => (
           <Step key={label}>
             <StepLabel>{label}</StepLabel>
+            <StepContent>
+              {getStepContent(index)}
+              <div className={classes.actionsContainer}>
+                <div>
+                  <Button
+                    disabled={activeStep === 0}
+                    onClick={handleBack}
+                    className={classes.button}
+                  >
+                    Back
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleNext}
+                    className={classes.button}
+                  >
+                    {activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+                  </Button>
+                </div>
+              </div>
+            </StepContent>
           </Step>
         ))}
       </Stepper>
-      <React.Fragment>
-        {activeStep === steps.length ? (
-          <React.Fragment>
-            <Typography variant="h5" gutterBottom>
-              Thank you for your order.
-                </Typography>
-            <Typography variant="subtitle1">
-              Your order number is #2001539. We have emailed your order confirmation, and will
-              send you an update when your order has shipped.
-                </Typography>
-          </React.Fragment>
-        ) : (
-            <React.Fragment>
-              {getStepContent(activeStep)}
-              <div className={classes.buttons}>
-                {activeStep !== 0 && (
-                  <Button onClick={handleBack} className={classes.button}>
-                    Back
-                    </Button>
-                )}
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={handleNext}
-                  className={classes.button}
-                >
-                  {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
-                </Button>
-              </div>
-            </React.Fragment>
-          )}
-      </React.Fragment>
+      {activeStep === steps.length && (
+        <Paper square elevation={0} className={classes.resetContainer}>
+          <Typography>All steps completed - you&apos;re finished</Typography>
+          <Button onClick={handleReset} className={classes.button}>
+            Reset
+          </Button>
+        </Paper>
+      )}
     </div>
   );
 }
