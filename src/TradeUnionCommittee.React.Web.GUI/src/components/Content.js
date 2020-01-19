add2fcf9046ba7bb4342fde796dd06178254a4ff
Serhii Zavada <zavada.serhii@outlook.com>
2020-01-19 19:28:53 +0200

    Added Copyright div

--- src/TradeUnionCommittee.React.Web.GUI/src/components/Content.js
+++ src/TradeUnionCommittee.React.Web.GUI/src/components/Content.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
 
 import Routes from '../routes'
 
@@ -22,14 +24,32 @@ const useStyles = makeStyles(theme => ({
         [theme.breakpoints.up(800 + theme.spacing(3) * 2)]: {
             margin: theme.spacing(5),
         },
-      },
-      paper: {
+    },
+    paper: {
         padding: theme.spacing(2),
         [theme.breakpoints.up(800 + theme.spacing(3) * 2)]: {
-          padding: theme.spacing(3),
+            padding: theme.spacing(3),
         }
-      },
+    },
+    copyright: {
+        paddingTop: theme.spacing(3)
+    },
 }));
+
+function Copyright(props) {
+    return (
+        <div className={props.style.copyright}>
+            <Typography variant="body2" color="textSecondary" align="center">
+                {'Copyright © '}
+                <Link color="inherit" href="/">
+                    Trade Union Committee React Web GUI - 
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        </div>
+    );
+}
 
 function Content() {
 
@@ -44,6 +64,7 @@ function Content() {
                         <Paper className={classes.paper}>
                             <Routes />
                         </Paper>
+                        <Copyright style={classes}/>
                     </Box>
                 </React.Fragment>
             </div>
