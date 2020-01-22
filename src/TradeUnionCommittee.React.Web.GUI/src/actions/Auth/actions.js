2fbcdc08bbd1fedaedaf6659f1bdfbf63cd73b6b
Zavada Serhii <zavadaserg@hotmail.com>
2020-01-22 17:27:37 +0200

    ActionTypes submitted to constants

--- src/TradeUnionCommittee.React.Web.GUI/src/actions/Auth/actions.js
+++ src/TradeUnionCommittee.React.Web.GUI/src/actions/Auth/actions.js
@@ -1,18 +1,16 @@
-export const AUTH_CHANGE_EMAIL_TEXT = 'AUTH_CHANGE_EMAIL_TEXT'
-export const AUTH_CHANGE_PASSWORD_TEXT = 'AUTH_CHANGE_PASSWORD_TEXT'
-export const AUTH_CHANGE_REMEMBER_ME_CHECKBOX = 'AUTH_CHANGE_REMEMBER_ME_CHECKBOX'
+import ActionTypes from '../../constants/actionTypes'
 
 export const setEmailText = email => ({
-    type: AUTH_CHANGE_EMAIL_TEXT,
+    type: ActionTypes.AUTH_CHANGE_EMAIL_TEXT,
     payload: email
 })
 
 export const setPasswordText = password => ({
-    type: AUTH_CHANGE_PASSWORD_TEXT,
+    type: ActionTypes.AUTH_CHANGE_PASSWORD_TEXT,
     payload: password
 })
 
 export const setRememberMeCheckbox = rememberMe => ({
-    type: AUTH_CHANGE_REMEMBER_ME_CHECKBOX,
+    type: ActionTypes.AUTH_CHANGE_REMEMBER_ME_CHECKBOX,
     payload: rememberMe
 })
--- src/TradeUnionCommittee.React.Web.GUI/src/reducers/Auth/reducers.js
+++ src/TradeUnionCommittee.React.Web.GUI/src/reducers/Auth/reducers.js
@@ -1,4 +1,4 @@
-import { AUTH_CHANGE_EMAIL_TEXT, AUTH_CHANGE_PASSWORD_TEXT, AUTH_CHANGE_REMEMBER_ME_CHECKBOX } from '../../actions/Auth/actions'
+import ActionTypes from '../../constants/actionTypes'
 
 const initialState = {
     email: '',
@@ -9,11 +9,11 @@ const initialState = {
 
 export default function authReducer(state = initialState, action) {
     switch (action.type) {
-        case AUTH_CHANGE_EMAIL_TEXT:
+        case ActionTypes.AUTH_CHANGE_EMAIL_TEXT:
             return { ...state, email: action.payload }
-        case AUTH_CHANGE_PASSWORD_TEXT:
+        case ActionTypes.AUTH_CHANGE_PASSWORD_TEXT:
             return { ...state, password: action.payload }
-        case AUTH_CHANGE_REMEMBER_ME_CHECKBOX:
+        case ActionTypes.AUTH_CHANGE_REMEMBER_ME_CHECKBOX:
             return { ...state, rememberMe: action.payload }
         default:
             return state
