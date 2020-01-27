8227245c53f22b2a3711016ab0b6c7a3a1f43a82
Serhii Zavada <zavada.serhii@outlook.com>
2020-01-27 20:21:34 +0200

    Formated document SplashScreen

--- src/TradeUnionCommittee.React.Web.GUI/src/components/SplashScreen.Component/SplashScreen.jsx
+++ src/TradeUnionCommittee.React.Web.GUI/src/components/SplashScreen.Component/SplashScreen.jsx
@@ -4,47 +4,28 @@ import React, { Component } from 'react';
 import './SplashScreen.css';
 
 function SplashScreen(WrappedComponent) {
+
   document.body.style.backgroundImage = 'radial-gradient(circle farthest-corner at center, #3C4B57 0%, #1C262B 100%)'
+
   return class extends Component {
+
     constructor(props) {
       super(props);
-      this.state = {
-        loading: true,
-      };
+      this.state = { loading: true };
     }
 
     async componentDidMount() {
       try {
-
-        // -----------------------------------------------------------------
-        // -- Emulation authorization (Check access token ...)
-        // -----------------------------------------------------------------
-
-        // await auth0Client.loadSession();
-        // await this.sleeper(1000);
-
-        // -----------------------------------------------------------------
-
-        setTimeout(() => {
-          this.setState({
-            loading: false,
-          });
-        }, 1500)
-        
-      } catch (err) {
-        console.log(err);
-        this.setState({
-          loading: false,
-        });
+        setTimeout(() => { this.setState({ loading: false }) }, 2000)
+      } catch (error) {
+        this.setState({ loading: false });
       }
     }
 
     render() {
-      // while checking user session, show "loading" message
       if (this.state.loading) return this.LoadingMessage();
-      
+
       document.body.style.backgroundImage = null;
-      // otherwise, show the desired route
       return <WrappedComponent {...this.props} />;
     }
 
@@ -56,10 +37,6 @@ function SplashScreen(WrappedComponent) {
           <div className="inner three"></div>
         </div>
       );
-    }
-
-    sleeper(ms) {
-      return new Promise(resolve => setTimeout(() => resolve(), ms));
     }
   };
 }
