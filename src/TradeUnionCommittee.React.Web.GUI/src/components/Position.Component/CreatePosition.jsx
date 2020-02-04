0e223ddcf6b0e46987f7ef77ea5b6d557cb38e65
Zavada Serhii <zavadaserg@hotmail.com>
2020-02-05 00:27:49 +0200

    Added form for create position

--- src/TradeUnionCommittee.React.Web.GUI/src/components/Position.Component/CreatePosition.jsx
+++ src/TradeUnionCommittee.React.Web.GUI/src/components/Position.Component/CreatePosition.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { withRouter } from "react-router-dom";
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 
 class CreatePosition extends React.Component {
 
     render() {
         return (
-            <div>
-                Create postion component
-            </div>
+            <React.Fragment>
+                <Typography variant="h5" align="left" gutterBottom>
+                    Create position
+                </Typography>
+                <Divider />
+                <br />
+                <Grid container spacing={3}>
+                    <Grid item xs={12}>
+                        <TextField
+                            required
+                            variant="outlined"
+                            name="name"
+                            label="Name position"
+                            fullWidth />
+                    </Grid>
+                    <Grid item xs={12} align="right">
+                        <Button variant="contained" color="primary">
+                            Create
+                        </Button>
+                    </Grid>
+                </Grid>
+            </React.Fragment>
         );
     }
 }
 
-export default withRouter(CreatePosition)
+export default CreatePosition
