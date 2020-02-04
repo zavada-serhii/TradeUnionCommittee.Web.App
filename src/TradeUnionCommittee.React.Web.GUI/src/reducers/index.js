136ecda34b042474f9c874923368998ba2e78497
Serhii Zavada <zavada.serhii@outlook.com>
2020-02-04 15:38:52 +0200

    Removed Counte rContainer

--- src/TradeUnionCommittee.React.Web.GUI/src/reducers/index.js
+++ src/TradeUnionCommittee.React.Web.GUI/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
-import counterReducer from './Counter/reducers'
 import authReducer from './Auth/reducers'
 import positionReducer from './Position/reducers'
 
 const rootReducer = combineReducers({
-  counter: counterReducer,
   auth: authReducer,
   position: positionReducer
 })
